1438aec462f059bccaefed465bbaf15325d3cf79
Madak <admin@madak.com>
2025-07-27 15:55:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ gsap.from(".home__hero", {
     y: "2rem",
     opacity: 0,
     duration: 1,
-    ease: "power2.out",
-    scrollTrigger: {
-      trigger: ".section.is--home__hero",
-      start: "top 80%", // when top of button hits 80% of viewport height
-      toggleActions: "play none none none"
-    }
+    ease: "power2.out"
   });
