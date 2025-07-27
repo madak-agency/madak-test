f7c385090db4983598d31c6bf996f7376f91a701
Madak <admin@madak.com>
2025-07-27 15:52:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-gsap.from(".cta-button", {
+gsap.from(".home__hero", {
     y: "2rem",
     opacity: 0,
     duration: 1,
     ease: "power2.out",
     scrollTrigger: {
-      trigger: ".cta-button",
+      trigger: ".home__hero",
       start: "top 80%", // when top of button hits 80% of viewport height
       toggleActions: "play none none none"
     }
